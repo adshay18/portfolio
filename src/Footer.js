8467b731de7275922815f878e6b00bc18c23d0f9
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-12-21 11:20:34 -0500

    finalize footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 const Footer = () => {
     return(
         <footer className="text-muted">
@@ -6,6 +7,10 @@ const Footer = () => {
                 <a className="text-white" href="/">Back to top</a>
                 </p>
                 <p>All work is &copy; Andrew Shay, please reach out to inquire about a custom website. <a target="_blank" rel="noreferrer" className="text-muted" href = "mailto:1996ashay@gmail.com"><i className="fa-solid fa-paper-plane"></i></a> <a className="text-muted" href="https://www.linkedin.com/in/andrewdshay/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a> <a href="https://github.com/adshay18" target="_blank" rel="noreferrer" className="text-muted"><i className="fa-brands fa-github"></i></a></p>
+                <p>
+                    Built in React
+                    <img src={logo} className="App-logo" alt="logo" />
+                </p>
             </div>
         </footer>
     )
