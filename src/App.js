60ef963624223af15fbca1f9becc43f118bd9077
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-12-21 11:18:13 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return(
+        <footer className="text-muted">
+            <div className="container">
+                <p className="float-right">
+                <a className="text-white" href="/">Back to top</a>
+                </p>
+                <p>All work is &copy; Andrew Shay, please reach out to inquire about a custom website. <a target="_blank" rel="noreferrer" className="text-muted" href = "mailto:1996ashay@gmail.com"><i className="fa-solid fa-paper-plane"></i></a> <a className="text-muted" href="https://www.linkedin.com/in/andrewdshay/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a> <a href="https://github.com/adshay18" target="_blank" rel="noreferrer" className="text-muted"><i className="fa-brands fa-github"></i></a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
